678ed483a254592b6df056cbcd315eca4f5ba884
gtsakiri <tsakiridis@serres.gr>
2022-07-04 09:05:50 +0300

    hi

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import { useNavigate, Link } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../context/AuthProvider";
+import Header from "./Header"
 
 const Home = () => {
     const { setAuth } = useContext(AuthContext);
@@ -15,6 +16,7 @@ const Home = () => {
 
     return (
         <section>
+            <Header />            
             <h1>Home</h1>
             <br />
             <p>You are logged in!</p>
